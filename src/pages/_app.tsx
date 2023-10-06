528150a2108792a3a382c31abddd65dc35cee142
Francois Huyghe <francois@13milliseconds.com>
2023-10-06 11:44:31 -0400

    cleanup css, fix hover link

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,25 +11,6 @@ export interface SharedPageProps {
 
 const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-family-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
-const sans = Inter({
-  variable: '--font-family-sans',
-  subsets: ['latin'],
-  weight: ['500', '700', '800'],
-})
-
-const serif = PT_Serif({
-  variable: '--font-family-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
-
 export default function App({
   Component,
   pageProps,
@@ -37,7 +18,7 @@ export default function App({
   const { draftMode, token } = pageProps
   return (
     <>
-      <style jsx global>
+      {/* <style jsx global>
         {`
           :root {
             --font-family-sans: ${sans.style.fontFamily};
@@ -45,7 +26,7 @@ export default function App({
             --font-family-mono: ${mono.style.fontFamily};
           }
         `}
-      </style>
+      </style> */}
       {draftMode ? (
         <PreviewProvider token={token}>
           <Component {...pageProps} />
