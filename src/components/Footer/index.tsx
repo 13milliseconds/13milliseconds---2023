2392db25f6f985b85f013448c6801ab1b5e92105
Francois Huyghe <francois@13milliseconds.com>
2023-10-07 10:59:31 -0400

    Marqee on footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,15 @@
+import Marquee from "react-fast-marquee";
+
 import styles from './styles.module.css'
+
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footer__content}>We’re based in Upstate New York. Time Zone EST. <a href="mailto:hello@13milliseconds.com" target='_blank'>hello@13milliseconds.com</a></div>
+      <Marquee autoFill pauseOnHover>
+      <div className={styles.footer__content}>We’re based in Upstate New York. Time Zone EST. <a href="mailto:hello@13milliseconds.com" target='_blank'>hello@13milliseconds.com</a></div>  
+      </Marquee>
+
     </footer>
   )
 }
